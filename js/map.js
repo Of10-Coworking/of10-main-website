eb229a608699296c63819f6c98a4a018e3837c84
usood <upahar.sood@gmail.com>
2016-05-16 19:23:45 +0530

    Removed home page image

--- js/map.js
+++ js/map.js
@@ -11,10 +11,8 @@ function init() {
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
         zoom: 19,
-
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(19.116067, 72.909791), // Prudential
-
         scrollwheel: false,
         navigationControl: false,
         mapTypeControl: false,
@@ -35,6 +33,5 @@ function init() {
         position: new google.maps.LatLng(19.115825, 72.909912),
         map: map,
         icon: 'img/map-marker.png'
-
     });
 };
