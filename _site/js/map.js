5fb1d4b84dd5438869ef96ab671c0b68815bd320
usood <upahar.sood@gmail.com>
2016-05-18 09:32:50 +0530

    Remove Keys

---
+++ _site/js/map.js
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------------->
+// MAP ELEMENT  ||-----------
+// ---------------------------------------------------------------------------------------------------------------------------->
+
+
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+    // Basic options for a simple Google Map
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 19,
+
+        // The latitude and longitude to center the map (always required)
+        center: new google.maps.LatLng(19.116067, 72.909791), // Prudential
+
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: false,
+        styles: [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"color":"#84afa3"},{"lightness":52}]},{"stylers":[{"saturation":-17},{"gamma":0.36}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]}]
+    };
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using our element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+
+    // Let's also add a marker while we're at it
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(19.115825, 72.909912),
+        map: map,
+        icon: 'img/map-marker.png'
+
+    });
+};
