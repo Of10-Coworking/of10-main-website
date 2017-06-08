d0b481ba2e94ce2cc49132a816de9155bfbe9a16
usood <upahar.sood@gmail.com>
2017-06-08 12:09:13 +0530

    More images compressed

--- js/custom.js
+++ js/custom.js
@@ -5,23 +5,22 @@ function init() {
     imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
     }
   }
-  var imgAmp = document.getElementById('of10-gif');
-  for (var i=0; i<imgDefer.length; i++) {
-    if(imgDefer[i].getAttribute('data-src')) {
-    imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
-    }
+  var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+  if (mobile) {
+    console.log("mobile");
+    document.getElementsByClassName
   }
 }
 window.onload = init;
-$("#imgAnimate").hover(
-    function()
-    {
-        $(this).attr("src", "img/brickwall/of10-shared-space-hustle.gif");
-    },
-    function()
-    {
-        $(this).attr("src", "img/brickwall/of10-shared-space-hustle.gif");
-    });
+// $("#imgAnimate").hover(
+//     function()
+//     {
+//         $(this).attr("src", "img/brickwall/of10-shared-space-hustle.gif");
+//     },
+//     function()
+//     {
+//         $(this).attr("src", "img/brickwall/of10-shared-space-hustle.gif");
+//     });
 function toggleOverlay() {
     console.log("Clicked");
     var toggle = document.getElementById("nav-toggle")
@@ -29,9 +28,9 @@ function toggleOverlay() {
     var el = document.getElementById("navigation");
     el.style.height = (el.style.height != '100%' ? '100%' : '0%' );
 }
-$('#datepicker').datepicker({
-  inline: true,
-  showOtherMonths: true,
-  dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-  dateFormat: 'd MM, y'
- });
+// $('#datepicker').datepicker({
+//   inline: true,
+//   showOtherMonths: true,
+//   dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+//   dateFormat: 'd MM, y'
+//  });
