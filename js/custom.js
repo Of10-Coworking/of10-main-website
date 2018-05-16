62a0d1df288409501b2f9541ca8ed31b4a8e235f
Of10 Labs <upaharsood@of10.in>
2018-05-16 20:06:06 +0530

    Fixed a contact page bug. Also added lazy loading logic for images to speed up website loading

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,4 @@
 function init() {
-  var imgDefer = document.getElementsByTagName('img');
-  for (var i=0; i<imgDefer.length; i++) {
-    if(imgDefer[i].getAttribute('data-src')) {
-    imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
-    }
-  }
   var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
   if (mobile) {
     console.log("mobile");
@@ -15,29 +9,17 @@ function init() {
   }else{
     var video = document.getElementById("of10-video");
     var gif = document.getElementById("of10-gif");
-    gif.style.display="none";
-    video.style.display = 'block';
+    if(gif !== null){
+      gif.style.display="none";
+      video.style.display = 'block';
+    }
   }
 }
 window.onload = init;
-// $("#imgAnimate").hover(
-//     function()
-//     {
-//         $(this).attr("src", "img/brickwall/of10-shared-space-hustle.gif");
-//     },
-//     function()
-//     {
-//         $(this).attr("src", "img/brickwall/of10-shared-space-hustle.gif");
-//     });
+
 function toggleOverlay() {
     var toggle = document.getElementById("nav-toggle")
     toggle.classList.toggle( "active" );
     var el = document.getElementById("navigation");
     el.style.height = (el.style.height != '100%' ? '100%' : '0%' );
 }
-// $('#datepicker').datepicker({
-//   inline: true,
-//   showOtherMonths: true,
-//   dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-//   dateFormat: 'd MM, y'
-//  });
