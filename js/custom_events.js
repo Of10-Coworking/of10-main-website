01204a8e9da3ddae239acdca647c61b4d6d9df0c
Of10 Labs <upaharsood@of10.in>
2018-12-08 15:11:29 +0530

    Updated google calendar stuff

--- js/custom_events.js
+++ js/custom_events.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-    $('#calendar').fullCalendar({
-        googleCalendarApiKey: 'AIzaSyBqm8nK6SR7DYVWWICWv7Enpua0H_0pw88',
-        defaultView: 'month',
-        timezone: 'India/Kolkata',
-        events: {
-            googleCalendarId: 'bhargav@of10.in'
-        },
-        eventColor: '#ec0089',
-        noEventsMessage: 'No events coming up this month',
-        eventClick: function(calEvent, jsEvent, view) {
-            console.log('Event: ' + calEvent.title);
-            console.log('JsEvent: ' + jsEvent);
-            console.log('View: ' + view.name);
-        }
-    });
+  $("#calendar").fullCalendar({
+    googleCalendarApiKey: "AIzaSyDKjprJcWyztXtA1aOABKQyVozAOEedekQ",
+    defaultView: "month",
+    timezone: "India/Kolkata",
+    events: {
+      googleCalendarId: "anam@of10.in"
+    },
+    eventColor: "#ec0089",
+    noEventsMessage: "No events coming up this month",
+    eventClick: function(calEvent, jsEvent, view) {
+      console.log("Event: " + calEvent.title);
+      console.log("JsEvent: " + jsEvent);
+      console.log("View: " + view.name);
+    }
+  });
 });
 
 function toggleOverlay() {
-    var toggle = document.getElementById("nav-toggle")
-    toggle.classList.toggle( "active" );
-    var el = document.getElementById("navigation");
-    el.style.height = (el.style.height != '100%' ? '100%' : '0%' );
+  var toggle = document.getElementById("nav-toggle");
+  toggle.classList.toggle("active");
+  var el = document.getElementById("navigation");
+  el.style.height = el.style.height != "100%" ? "100%" : "0%";
 }
